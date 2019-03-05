e5b255186ccd52f4f11abe37db697cf90812eb15
Paul Nykiel <paul.nykiel@gmail.com>
2019-03-05 13:39:17 +0100

    Added CI

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "On Branch: $TRAVIS_BRANCH"
+
+#deploy gh-pages
+echo '[deploy_gh-pages]: create new temp dir'
+mkdir deployGH-PAGES
+cd deployGH-PAGES
+
+echo '[deploy_gh-pages]: clone gh-pages branch'
+git clone -b gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git
+export TRAVIS_REPO_NAME=${TRAVIS_REPO_SLUG#*/}
+cd ${TRAVIS_REPO_NAME}
+
+echo '[deploy_gh-pages]: configuring git'
+git config --global push.default simple
+git config user.name "Travis CI"
+git config user.email "deploy@travis-ci.org"
+
+echo '[deploy_gh-pages]: copy new files'
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    rm -f *
+    cp ../../output/* .
+else
+    rm -rf "$TRAVIS_BRANCH"
+    mkdir -p "$TRAVIS_BRANCH"
+    cp ../../output/* ./"$TRAVIS_BRANCH"/
+fi
+
+echo '[deploy_gh-pages]: git add & commit'
+git add --all
+git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+
+echo '[deploy_gh-pages]: git push'
+git push --force "https://${GH_REPO_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
+
+echo '[deploy_gh-pages]: remove temp dir'
+cd ../..
+rm -rf deployGH-PAGES
